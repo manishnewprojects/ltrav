16816503f608055c3db4a4cc830ff7e6d31185c4
ManishV <manishnewprojects@gmail.com>
2018-09-05 06:51:28 -0700

    images

---
+++ htdocs/cesky_krumlov.php
@@ -0,0 +1,127 @@
+<!-- Copyright (C) 2018, LanternTavels.com -->
+
+<!DOCTYPE html>
+
+<html lang="en">
+ 
+<head>
+
+<?php require( 'config.php' ); ?>
+
+<?php include (VIEW_HEADER); ?>
+
+</head>
+
+
+<!-- Primary Page Layout -->
+
+<body>
+
+<?php include (VIEW_NAVIGATION) ; ?>
+
+<main role="main" class="inner cover">
+
+<div class="container">
+ <?php include (DIR_INCL . 'google_ad_top_horizonal.php'); ?>  
+      <!-- Page Heading -->
+      <div align="center">
+      <h1 class="my-4">
+        <br>Cesky Krumlov, Czechia<br>
+       </h1>
+      </div>
+      
+      <div class="row">
+        <div class="col-lg-9 col-sm-12 portfolio-item">
+          <div class="card h-100">
+           
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Castle, Dungeon, Village, River. Not a backlot.</a>
+              </h4>
+              <p class="card-text">Cesky Krumlov, Czechia
+
+                <!-- WP TEXT BEGINS -->
+
+                 
+                 <p class="card-text @font-face">
+
+                 <b><i><u>TL;DR</u>: Must do day trip from Prague. Take the train.  Enjoy the castle museum. View from the tower is awesome. Pick up sandwiches from local grocery store in Cesky Krumlov. Avoid the touristy restaurants.</i></b>
+                  
+                  <br><br>
+
+                  Cesky Krumlov, one of the most fascinating medieval towns that I have visited. We took the early morning train from the main station in Prague. It leaves around 8:00 in the morning, and we were able to easily get tickets at the station.
+
+                    <img class="mv_image" src="images/cesky_krumlov/prague_train_to_cesky_krumlov.jpg" alt="Prague cesky krumlov train"/>
+
+                   Everybody in Prague seems to pick up the bread fresh every morning and we did the same from a store at the station. This came in handy to fix a nice breakfast in the morning. The train arrived exactly on time and we boarded without much difficulty. We were there in April, which I guess is not the high tourist season, so the train was not a very crowded. We had a whole compartment to ourselves, which was really fantastic. The train goes by beautiful Czech countryside offering some great views. I was amused by the guys walking down the aisle with a trolly selling tea, coffee and biscuits.  Three hours go by really fast. One of the interesting stops along the way is the town of Brno. For those interested in the history of genetics, the monastery in this town was where Gregor Mendel conducted his famous experiments on evolution using pea plants.
+
+                  The train rolls into Cesky Krumlov around 11:00. A very small, quaint station, which I loved right away.  
+
+                  <img class="mv_image" src="images/cesky_krumlov/cesky_krumlov_train_station.jpg" alt="station cesky krumlov train"/>
+
+                  For such a tourist destination, you would think that there would be lots of buses and taxis outside the station, but that wasn't the case at all. Which is just as well because its only about a 15 minute walk down to the castle. The walk takes you up and down a hill, along a a very picturesque path. As you are walking down the hill, you get a beautiful view of the castle and the village below. 
+
+                  <img class="mv_image" src="images/cesky_krumlov/cesky_krumlov_view_from_station_road.jpg" alt="station cesky krumlov train"/>
+
+
+                  A short walk later you find yourself looking up at this beautiful aqueduct leading up to the main castle. As you go under the aqueduct and over an idyllic bridge on the river, and you find yourself right in town, which in itself is almost frozen in the 14th century. 
+
+                  <img class="mv_image" src="images/cesky_krumlov/cesky_krumlov_aquaduct.jpg" alt="acquaduct cesky krumlov"/>
+
+
+                  You can get a ticket to visit the whole castle or parts of the castle. Start by going through the small museum that's right by the entrance (easy to miss, look for museum signs). 
+                  <img class="mv_image" src="images/cesky_krumlov/cesky_krumlov_castle_from_the_village.jpg" alt="village cesky krumlov"/>
+
+                  The museum has lots of interesting artifacts around the life of the “Lords of Krumlov”, like the Rosenbergs who were ranked among the most significant and influential Czech noble families. The life they lived in the country, and overall history of the region. One of the most fascinating parts of the castle for me was going up the tower. This was painstakingly restored some years ago by volunteers and, and looks really fantastic. From top of the tower you get a view that's unforgettable. Almost like being on a grand movie set or like just having walked right into a painting from the 14th century. 
+                  <img class="mv_image" src="images/cesky_krumlov/cesky_krumlov_view_from_castle.jpg" alt="castle tower cesky krumlov"/>
+
+
+                  Looking out from top of the tower you see a river that flows over quant bridges and along a small town.  You could almost imagine looking down on people going about their daily business like they did hundreds of years agi.
+                  The tower even has a real dungeon in the tower, which presumably held prisoners at one point.
+                  You can spend couple of hours walking around the castle area, visiting the tower and the town next to it. Some parts of the town would clearly rebuilt, for the tourists. These parts look more like a Disneyland version. You can conveniently skip those and just focus on the real artifacts.  
+
+                  <img class="mv_image" src="images/cesky_krumlov/cesky_krumlov_village_street.jpg" alt="village street cesky krumlov"/>
+
+                  You can make your way back to the station for the 2:00 PM train back to Prague. We also picked up some groceries in town for sandwiches on the journey back. And the ride back is again smooth. You get to see the countryside all over again and will be back to Prague, at around a five in the evening leaving the evening free for other activities.   
+
+
+
+
+
+
+                <!-- WP TEXT ENDS -->
+
+              </p>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-lg-3 col-sm-6 portfolio-item">
+                      <?php include 'php_includes/google_ad_336x280.php'; ?>  
+
+          <h4 class="card-title">
+          <!-- <div class="card h-100 border border-primary">
+                <a href="interlaken.php"><img class="card-img-top" src="images/switzerland/switzerland-interlaken-700.jpg" alt=""></a>
+                <div class="card-body">
+                  <h4 class="card-title">
+                    <a href="interlaken.php">Interlaken, Switzerland</a>
+                  </h4>
+                  <p class="card-text">How green is my valley?</p>
+                </div>
+              </div> -->
+        </div>
+        
+      </div>
+      <!-- /.row -->
+  </div>
+<!-- /.container -->
+</main>
+
+<?php include 'php_includes/footer.php'; ?>
+
+
+<?php include 'php_includes/end_js.php'; ?>
+
+</body>
+</html>
